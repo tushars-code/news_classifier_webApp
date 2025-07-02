f6437cedb0ba3b69ef78d6ebb9d6f4fd45dce8e1
tushars-code <tusharyerne7d2@gmail.com>
2025-07-02 19:48:19 +0530

    1.0

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -4,7 +4,7 @@ function NewsList() {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/news")
+    fetch("https://news-classifier-webapp-backendmodel.onrender.com/news")
       .then((res) => res.json())
       .then((data) => {
         setNews(data.news);
